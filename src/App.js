9b2d4f681ce3fd98210c886f888df8a2b377b08c
venugopalreddy123 <venu641995@gmail.com>
2020-07-28 14:32:06 +0530

    Removed all duplicates by using methods

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
 import React from "react";
 import { Singers } from "./Singers/Singers";
+import { Singerslist } from "./Singers/Singerslist";
 function App() {
   return (
     <div className="App">
-      <h1
-        style={{
-          margin: "auto",
-          textAlign: "center",
-          marginTop: "20px",
-
-          width: "1200px",
-          height: "100px",
-          backgroundColor: "darkblue",
-        }}
-      >
-        Hello welcome to singers App{" "}
-      </h1>
-      <Singers
-        banner="https://1847884116.rsc.cdn77.org/telugu/gallery/actress/geethamadhuri/Geetha220814_041.jpg"
-        name="Geetha Madhuri"
-        details="Indian playback singer"
-      />
-      <Singers
-        banner="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQmVTgn-X3RcvTtzY0nUKiyPNtbsWYX_af4HA&usqp=CAU"
-        name="Dhvai Bhanusali"
-        details="Indian playback singer"
-      />
-      <Singers
-        banner="https://www.topplanetinfo.com/wp-content/uploads/2018/08/Shreya-Ghoshal.jpg"
-        name="Shreya Ghoshal"
-        details="Indian playback singer"
-      />
-      <Singers
-        banner="https://1847884116.rsc.cdn77.org/telugu/gallery/actress/geethamadhuri/Geetha220814_041.jpg"
-        name="Geetha Madhuri"
-        details="Indian playback singer"
-      />
-      <Singers
-        banner="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQmVTgn-X3RcvTtzY0nUKiyPNtbsWYX_af4HA&usqp=CAU"
-        name="Dhvai Bhanusali"
-        details="Indian playback singer"
-      />
-      <Singers
-        banner="https://www.topplanetinfo.com/wp-content/uploads/2018/08/Shreya-Ghoshal.jpg"
-        name="Shreya Ghoshal"
-        details="Indian playback singer"
-      />
+      <Singerslist />
     </div>
   );
 }
